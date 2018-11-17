6e0ee0f630c3ce51692894e23125ce1c6f0e0deb
Joseph Gonzalez Pastora <josegonzalez2330@gmail.com>
2018-11-17 01:14:51 -0600

    Se agrega un gitignore y se empieza el RAID

---
+++ RAID.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joseph González Pastora on 11/17/18.
+//
+
+#include "RAID.h"
---
+++ RAID.h
@@ -0,0 +1,14 @@
+//
+// Created by Joseph González Pastora on 11/17/18.
+//
+
+#ifndef TECMEDIAFILESYSTEM_RAID_H
+#define TECMEDIAFILESYSTEM_RAID_H
+
+
+class RAID {
+
+};
+
+
+#endif //TECMEDIAFILESYSTEM_RAID_H
